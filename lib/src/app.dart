9658a913561330c244922939e585b27fa992fc15
JavierDuranFlores <duranfloresj7@gmail.com>
2023-09-26 11:31:20 -0600

    huella_id

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:app_2/src/inicio.dart';
 import 'package:flutter/services.dart';
 import 'package:local_auth/local_auth.dart';
-import 'package:local_auth_ios/local_auth_ios.dart';
 import 'dart:io';
 
 //clase principal, se manda a llamar desde el main
